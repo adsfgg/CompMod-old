c70466a1f4e8fbf37baf0fe98c9e0650c38fc8ce
Joseph Hutchins <joseph.hutchins@engageft.com>
2019-07-20 09:41:53 -0700

    Remove conflicting code related to grenades.

--- lua/CompMod/Marine/Weapons/Grenades/GrenadeQuickThrow/Post/Weapons/Marine/GrenadeThrower.lua
+++ lua/CompMod/Marine/Weapons/Grenades/GrenadeQuickThrow/Post/Weapons/Marine/GrenadeThrower.lua
@@ -1,6 +1,8 @@
 local networkVars =
 {
     grenadesLeft = "integer (0 to ".. kMaxHandGrenades ..")",
+    pullPinOnDeploy = "private boolean",
+    throwASAP = "private boolean",
     quickThrown = "boolean"
 }
 
